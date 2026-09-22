91783afc364ae11079073e579c058bc5a6ac3572
artizens <53269365+artizens@users.noreply.github.com>
2026-09-22 12:20:29 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
+
 const config: NextConfig = {
   output: 'export',
+  basePath: process.env.GITHUB_PAGES === 'true' ? '/MBTI-Test' : '',
   trailingSlash: true,
   images: { unoptimized: true },
 };
+
 export default config;
